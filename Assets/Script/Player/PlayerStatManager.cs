28a7b050c73477bc2214368075127fc0f5b3b52b
Longboye99 <144575154+Longboye99@users.noreply.github.com>
2025-01-31 19:49:54 +0700

    Add stamina tracker

---
+++ Assets/Script/Player/PlayerStatManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatManager : MonoBehaviour
+{
+    public float maxStamina;
+    private float currentStamina;
+    [SerializeField] float staminaDrain;
+    [SerializeField] Slider staminaSlider;
+
+    private void Start()
+    {
+        staminaSlider.maxValue = maxStamina;
+        staminaSlider.value = maxStamina;
+        currentStamina = maxStamina;
+    }
+
+    private void Update()
+    {
+        
+            currentStamina -= staminaDrain * Time.deltaTime;
+            staminaSlider.value = currentStamina;
+            Debug.Log(currentStamina);
+        
+    }
+
+}
